fdc3c5ceb55f39e0289ad6eade1e9fb8042c42b0
Gerwyn-George <62806334+Gerwyn-George@users.noreply.github.com>
2020-12-29 17:05:30 +0000

    Initial

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-import requests
+from urllib.request import urlopen
+ 
 
-r = requests.get("https://www.youtube.com/watch?v=E_xbHTPwhIQ")
+def Collect_Webpage(url):
+    page = urlopen(url)
+    html = page.read().decode("utf-8")
 
-print(r)
+    for line in html:
+        print("hello")
+
+    print(html)
+
+
+    
+Collect_Webpage("https://docs.python.org/3/library/urllib.parse.html#module-urllib.parse")
