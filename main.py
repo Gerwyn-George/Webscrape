aa74a1e01f6e4239cc906d559bb9a1fbdff6d975
Gerwyn George <gerwyn@kali.home>
2020-12-30 18:27:17 +0000

    Initial

--- main.py
+++ main.py
@@ -1,36 +1,17 @@
 import requests
-import re
+from bs4 import BeautifulSoup
 
+#user agent must be specified if not the website will refuse connection. 
 
+user_agent = { "User-Agent" : "Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0"}
+url = "https://www.tesco.com/groceries/en-GB/shop/fresh-food/all"
 
-  
+response = requests.get(url,headers=user_agent)
 
-headers ={ "user-agent": "mozilla/5.0"}
-res = requests.get("https://www.tesco.com/groceries/en-GB/shop/drinks/all", headers=headers)
+print(response)
+#print(response.content)
 
-try:
+page = BeautifulSoup(response.content, "html.parser")
 
-    for line in res:
-        print(line)
-    
-        x = re.findall("</html>",line)
-        if x:
-            print(x)
-        else:
-        pass 
-
-except:
-
-    print("Nevermind")
-
-
-
-
-    
-
-
-
-print(res.status_code)
-  
-
-
+for item in page.find_all(attrs={"data-auto":"product-tile--title"}):
+    print item
