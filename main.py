0cf76742c8cde8354950879298f9ea7fda5aa645
Gerwyn George <gerwyn@kali.home>
2021-01-03 23:54:08 +0000

    Initial

--- main.py
+++ main.py
@@ -12,41 +12,52 @@ url = "https://www.tesco.com/groceries/en-GB/shop/fresh-food/all"
 product = []
 price = []
 
-def get_product_name(page):
+class Main_Application:
+    pass
 
-    for item in page.find_all(attrs={"data-auto":"product-tile--title"}):
+class Sainsburys(Main_Application):
+    pass
 
-        product.append(item.text)
+class Tesco(Main_Application):
+
+    def get_product_name(self,page):
+
+        for item in page.find_all(attrs={"data-auto":"product-tile--title"}):
+
+            product.append(item.text)
         
-    return product
+        return product
 
-def get_product_price(page):
+    def get_product_price(self,page):
 
-    for item in page.find_all(attrs={"data-auto":"price-value"}):
+        for item in page.find_all(attrs={"data-auto":"price-value"}):
       
-        price.append(item.text)
-    return price
+            price.append(item.text)
+        return price
 
-def check_product_range(url):
+    def check_product_range(self,url):
 
-    original_url = url + "?page="
+        original_url = url + "?page="
 
-    for i in range(1,999):
+        for i in range(1,999):
 
-        url = original_url + str(i) 
+            url = original_url + str(i) 
          
-        response = requests.get(url,headers=user_agent)
-        page = BeautifulSoup(response.content, "html.parser")
+            response = requests.get(url,headers=user_agent)
+            page = BeautifulSoup(response.content, "html.parser")
         
-        sleep(1)
-        if response.status_code == 200:
-            print(url)
-            get_product_name(page)
+            sleep(1)
+            if response.status_code == 200:
+                print(url)
+                self.get_product_name(page)
+            
            
-        else:
-            break 
+            else:
+                break 
 
-check_product_range("https://www.tesco.com/groceries/en-GB/shop/fresh-food/all") 
+Tesco = Tesco()
+
+Tesco.check_product_range("https://www.tesco.com/groceries/en-GB/shop/fresh-food/all") 
     
 print(product)
 #get_product_name(page)
